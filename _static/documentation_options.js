72ba3cce1bba54c5f3afc74b44ed434ea604bd4d
Graham Markall <gmarkall@nvidia.com>
2020-04-14 23:20:41 +0100

    Use docs that include compile_ptx

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.49.0dev0+848.g3bc439ad5',
+    VERSION: '0.50.0dev0+89.g238c2ecb7.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
